aead4c0732beeb8a21c4f94a9435d24d7be9c782
Hallodri <bhallodri@gmail.com>
2021-02-20 19:14:51 +0300

    Main.java added

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int rate = 3;
+        int sum = 1000;
+        int Income = rate * sum / 100;
+        System.out.println(Income);
+    }
+}
+
